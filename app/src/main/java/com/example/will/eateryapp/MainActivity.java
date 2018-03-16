0cb4f3a4ec2220e72077a54f9d5b5e1edc3e5311
wlong123 <wlong2017@pingry.org>
2018-03-15 21:44:36 -0400

    trying to add map. added button

--- app/src/main/java/com/example/will/eateryapp/MainActivity.java
+++ app/src/main/java/com/example/will/eateryapp/MainActivity.java
@@ -2,6 +2,11 @@ package com.example.will.eateryapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+/*
+    public View onCreateView(LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.activity_main, container, false);
+        final Button b = v.findViewById(R.id.create_group);
+
+        b.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v)
+            {
+                Toast.makeText(getApplicationContext(), "To Delete a List, Hold Down on the Button",
+                        Toast.LENGTH_SHORT).show();
+            }
+
+        });
+
+        return v;
+    }
+    */
+
+    public void onClick(View v){
+        Toast.makeText(getApplicationContext(), "To Delete a List, Hold Down on the Button",
+                Toast.LENGTH_SHORT).show();
     }
 }
