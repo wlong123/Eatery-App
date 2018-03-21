6566a81bef604b7397d96b19428e2f06ff87c242
wlong123 <wlong2017@pingry.org>
2018-03-21 14:30:18 -0400

    commit

--- app/src/main/java/com/example/will/eateryapp/MainActivity.java
+++ app/src/main/java/com/example/will/eateryapp/MainActivity.java
@@ -7,13 +7,24 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.Toast;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import android.util.Log;
+import android.location.Location;
+import android.Manifest.permission;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private FusedLocationProviderClient mFusedLocationClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
     }
 /*
     public View onCreateView(LayoutInflater inflater,
@@ -39,5 +50,31 @@ public class MainActivity extends AppCompatActivity {
     public void onClick(View v){
         Toast.makeText(getApplicationContext(), "To Delete a List, Hold Down on the Button",
                 Toast.LENGTH_SHORT).show();
+        getLastLocation();
     }
+
+    public void getLastLocation(){
+        try {
+            mFusedLocationClient.getLastLocation()
+                    .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                        @Override
+                        public void onSuccess(Location location) {
+                            // Got last known location. In some rare situations this can be null.
+                            if (location != null) {
+                                Toast.makeText(getApplicationContext(), "failed",
+                                        Toast.LENGTH_SHORT).show();
+                                // Logic to handle location object
+                                //Log.d(location.toString(),"gotLocation");
+                                Toast.makeText(getApplicationContext(), location.toString(),
+                                        Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+        }
+        catch(SecurityException e){
+            Log.d("Failed","User Permission not Enabled");
+        }
+
+    }
+
 }
