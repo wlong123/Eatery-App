b448ca254e7915377cd43c2d79478e29c5359e16
wlong123 <wlong2017@pingry.org>
2018-03-28 18:50:26 -0400

    location permissions working

--- app/src/main/java/com/example/will/eateryapp/MainActivity.java
+++ app/src/main/java/com/example/will/eateryapp/MainActivity.java
@@ -14,18 +14,34 @@ import com.google.android.gms.tasks.OnSuccessListener;
 import android.util.Log;
 import android.location.Location;
 import android.Manifest.permission;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.app.ActivityCompat;
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.app.Activity;
+import android.support.design.widget.Snackbar;
 
 
 public class MainActivity extends AppCompatActivity {
 
     private FusedLocationProviderClient mFusedLocationClient;
 
+    private static final int REQUEST_FINE_LOCATION = 100;
+    private static final int REQUEST_CAMERA = 0;
+    private static final int REQUEST_CONTACTS = 1;
+
+    private View mLayout;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+
+        mLayout = findViewById(R.id.main_layout);
     }
+
 /*
     public View onCreateView(LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -50,22 +66,26 @@ public class MainActivity extends AppCompatActivity {
     public void onClick(View v){
         Toast.makeText(getApplicationContext(), "To Delete a List, Hold Down on the Button",
                 Toast.LENGTH_SHORT).show();
-        getLastLocation();
+        requestLocationPermission();
+        //getLastLocation();
     }
 
     public void getLastLocation(){
+        Log.d("success", "success");
         try {
             mFusedLocationClient.getLastLocation()
                     .addOnSuccessListener(this, new OnSuccessListener<Location>() {
                         @Override
                         public void onSuccess(Location location) {
                             // Got last known location. In some rare situations this can be null.
+                            //Log.d("success", location.toString());
                             if (location != null) {
-                                Toast.makeText(getApplicationContext(), "failed",
-                                        Toast.LENGTH_SHORT).show();
+                                //Toast.makeText(getApplicationContext(), "failed",
+                                  //      Toast.LENGTH_SHORT).show();
                                 // Logic to handle location object
                                 //Log.d(location.toString(),"gotLocation");
-                                Toast.makeText(getApplicationContext(), location.toString(),
+                                Toast.makeText(getApplicationContext(), location.
+                                                getLatitude() + ", " + location.getLongitude(),
                                         Toast.LENGTH_SHORT).show();
                             }
                         }
@@ -77,4 +97,122 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+/*
+    public void requestLocationPermission(){
+        // Here, thisActivity is the current activity
+        if (ContextCompat.checkSelfPermission(this,
+            Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                ContextCompat.checkSelfPermission( getApplicationContext(),
+                        android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED)
+        {
+
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.ACCESS_FINE_LOCATION)) {
+
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+
+                android.support.design.widget.Snackbar.make(mLayout, R.string.permission_camera_rationale,
+                        Snackbar.LENGTH_INDEFINITE)
+                        .setAction(R.string.ok, new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                                ActivityCompat.requestPermissions(MainActivity.this,
+                                        new String[]{Manifest.permission.CAMERA},
+                                        REQUEST_CAMERA);
+                            }
+                        })
+                        .show();
+
+
+            } else {
+
+                // No explanation needed, we can request the permission.
+
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.READ_CONTACTS},
+                        REQUEST_FINE_LOCATION);
+
+                // MY_PERMISSION_REQUEST_READ_FINE_LOCATION is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+            }
+        }
+    }
+*/
+
+    public void requestLocationPermission(){
+        // Here, thisActivity is the current activity
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                ContextCompat.checkSelfPermission( getApplicationContext(),
+                        android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED)
+        {
+
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.ACCESS_FINE_LOCATION)) {
+
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+
+                    Snackbar.make(mLayout, R.string.permission_location_rationale,
+                        Snackbar.LENGTH_INDEFINITE)
+                        .setAction(R.string.ok, new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                                ActivityCompat.requestPermissions(MainActivity.this,
+                                        new String[]{permission.ACCESS_FINE_LOCATION},
+                                        REQUEST_FINE_LOCATION);
+                            }
+                        })
+                        .show();
+
+
+            } else {
+
+                // No explanation needed, we can request the permission.
+
+                ActivityCompat.requestPermissions(this,
+                        new String[]{permission.ACCESS_FINE_LOCATION},
+                        REQUEST_FINE_LOCATION);
+
+                // MY_PERMISSION_REQUEST_READ_FINE_LOCATION is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+            }
+        }
+        getLastLocation();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_FINE_LOCATION: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted, yay! Do the contacts-related task you need to do.
+                    getLastLocation();
+
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
+    }
+
 }
+
+
